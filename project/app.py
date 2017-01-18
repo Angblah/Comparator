a6eb40a1060e8f3b156fb560fdb92c59e9611b82
Honey Chawla <hchawla6@gatech.edu>
2017-01-18 14:30:59 -0500

    I'm about to pull my hair out.

--- project/app.py
+++ project/app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask.ext.heroku import Heroku
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://wkwgmorgzmgqal:7006e7788fd04d1bf0a59d8dee3484fb6543b6acb3eba5fa5cdcad7129f53ca5@ec2-54-243-185-99.compute-1.amazonaws.com:5432/d8e0903afn75gv"
 heroku = Heroku(app)
 app.debug = True
 db = SQLAlchemy(app)
