157cfaf3272f3918cf09e73b3fe12ab7ffbff687
Honey Chawla <hchawla6@gatech.edu>
2017-01-19 21:41:50 -0500

    Connecting to Heroku's database! :D

--- app.py
+++ app.py
@@ -2,16 +2,11 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-print 1
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://postgres:byteme@localhost/the-comparator"
-#"postgres://lnzcnupimmpfyc:9bc06b432fef526c4cfc3844633637a6fcd1214bce2bd885b156f3a2cf099542@ec2-23-21-46-94.compute-1.amazonaws.com:5432/d26clo9dh5i9sp"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://lnzcnupimmpfyc:9bc06b432fef526c4cfc3844633637a6fcd1214bce2bd885b156f3a2cf099542@ec2-23-21-46-94.compute-1.amazonaws.com:5432/d26clo9dh5i9sp"
 #"postgres://postgres:byteme@localhost/the-comparator"
 app.debug = True
 db = SQLAlchemy(app)
-
-print 2
 
 from models import *
 
@@ -36,12 +31,7 @@ def add_user():
 	db.session.commit()
 	return redirect(url_for('index'))
 
-print 3
-
 if __name__ == '__main__':
-	print 4
-	print 5
 	port = int(os.environ.get('PORT', 5000))
-	print 6
 	print port
 	app.run(host='0.0.0.0', port=port, debug=True)
