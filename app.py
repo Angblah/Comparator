1021c2873ad9337cd0dcc1c07fb30f00094b9cf8
Honey Chawla <hchawla6@gatech.edu>
2017-01-19 21:09:01 -0500

    Adding print statements!

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
+print 1
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://postgres:byteme@localhost/the-comparator"
 #"postgres://lnzcnupimmpfyc:9bc06b432fef526c4cfc3844633637a6fcd1214bce2bd885b156f3a2cf099542@ec2-23-21-46-94.compute-1.amazonaws.com:5432/d26clo9dh5i9sp"
 #"postgres://postgres:byteme@localhost/the-comparator"
 app.debug = True
 db = SQLAlchemy(app)
+
+print 2
 
 from models import *
 
@@ -31,8 +35,15 @@ def add_user():
 	db.session.commit()
 	return redirect(url_for('index'))
 
+print 3
+
 if __name__ == '__main__':
+	print 4
+
     app.run(debug=True)
+    print 5
     port = int(os.environ.get('PORT', 5000))
+    print 6
     print port
     app.run(host='0.0.0.0', port=port)
+    print 7
