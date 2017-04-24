49bb793f3ebb39a7766ace34ce65a4b6f72655ef
Alex Wu <awu68@gatech.edu>
2017-04-23 23:55:03 -0400

    recent comparison limit set to 4

--- project/app.py
+++ project/app.py
@@ -352,7 +352,7 @@ def dashboard():
         return redirect(url_for('index'))
 
     # TODO: consider sorting all_comp in python for recent_comp (though sorting likely faster on database side through indices, returning both recent_comp and all_comp is inefficient)
-    recent_comp = get_recent_user_comparisons(current_user.id, 6, get_json=False)
+    recent_comp = get_recent_user_comparisons(current_user.id, 4, get_json=False)
     all_comp = get_user_comparisons(current_user.id, get_json=False)
     all_temp = get_user_templates(current_user.id, get_json=False)
 
